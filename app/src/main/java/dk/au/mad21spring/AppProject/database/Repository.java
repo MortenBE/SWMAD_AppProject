03fd982514b88f004cd044ffb6bf869979026b3e
LucifersUncle <au600586@uni.au.dk>
2021-04-29 15:15:50 +0200

    Firebase added

---
+++ app/src/main/java/dk/au/mad21spring/AppProject/database/Repository.java
@@ -0,0 +1,8 @@
+package dk.au.mad21spring.AppProject.database;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Repository {
+    //auth goes here
+    private FirebaseFirestore db;
+}
