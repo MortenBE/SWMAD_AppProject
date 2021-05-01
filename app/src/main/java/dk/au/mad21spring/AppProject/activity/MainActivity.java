efba7eb385830a4134ae1549ee239e86453e069b
MortenBE <Ehlersmorten@gmail.com>
2021-05-01 22:13:20 +0200

    Tilføjet location permision

--- app/src/main/java/dk/au/mad21spring/AppProject/activity/MainActivity.java
+++ app/src/main/java/dk/au/mad21spring/AppProject/activity/MainActivity.java
@@ -1,10 +1,12 @@
 package dk.au.mad21spring.AppProject.activity;
 
 
-
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,6 +15,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
@@ -27,6 +31,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Button QuizButton, ScoreButton;
     private GoogleMap mMap;
 
+    private boolean permissionGranted;
+    public static final int PERMISSIONS_REQUEST_LOCATION = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +45,46 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         initWigdet();
         initButtons();
+
+        checkLocationPermission();
+        setCurrentLocation();
+
     }
 
+    private void setCurrentLocation() {
+    }
 
+    //https://developer.android.com/training/permissions/requesting.html
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_LOCATION);
+        }
+    }
+
+    //mhttps://developer.android.com/training/permissions/requesting.html
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    // got permission
+
+                }
+                else {
+                    // permission denied
+                    Toast.makeText(this, "You need to enable permission for Location to use the app", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
+    }
 
     private void initWigdet() {
         QuizButton = findViewById(R.id.MainQuizButton);
@@ -91,6 +135,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 .title("Marker in Sydney"));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
 }
