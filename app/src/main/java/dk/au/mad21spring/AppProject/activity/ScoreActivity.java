0577b4cf41e5ce6b781b6ef51cc7dc41489e9e37
MortenBE <Ehlersmorten@gmail.com>
2021-05-12 22:33:15 +0200

    added onRestoreInstanceState to scoreactivity

--- app/src/main/java/dk/au/mad21spring/AppProject/activity/ScoreActivity.java
+++ app/src/main/java/dk/au/mad21spring/AppProject/activity/ScoreActivity.java
@@ -1,5 +1,6 @@
 package dk.au.mad21spring.AppProject.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,6 +19,7 @@ public class ScoreActivity extends AppCompatActivity {
     private static final String TAG = "ScoreActivity";
 
     ScoreViewModel scoreViewModel;
+    String quizId;
 
     private List<Score> highscores;
 
@@ -26,22 +28,22 @@ public class ScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
+        //Get quizId
+        if(getIntent().hasExtra("quizId")) {
+            quizId = getIntent().getStringExtra("quizId");
+        } else if(savedInstanceState != null) {
+            savedInstanceState.getString("quizId");
+        }
+
         //Init ViewModel
         scoreViewModel = new ViewModelProvider(this).get(ScoreViewModel.class);
+        //Setup observer for quiz highscores
+        scoreViewModel.getScores(quizId).observe(this, scores -> {
+            highscores = scores;
 
-        if(getIntent().hasExtra("quizId"))
-        {
-            //Get quizId
-            String quizId = getIntent().getStringExtra("quizId");
-
-            //Setup observer for quiz highscores
-            scoreViewModel.getScores(quizId).observe(this, scores -> {
-                highscores = scores;
-
-                //Do something with the scores
-                updateUI();
-            });
-        }
+            //Do something with the scores
+            updateUI();
+        });
     }
 
     private void initWidgets(){
@@ -53,5 +55,16 @@ public class ScoreActivity extends AppCompatActivity {
         {
             Toast.makeText(this, (j + 1) + ": Player: " + highscores.get(j).quizzersName + ", Score: " + highscores.get(j).getScore(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("quizId", quizId);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
